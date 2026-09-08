022332e3ed4b92b0e051748e9e2ee9af04855f4d
pedrohelias <pedrohelias95@hotmail.com>
2026-09-08 12:35:27 -0300

    adicoes

---
+++ prisma/migrations/20260908151625_add/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Professional" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "spec" TEXT NOT NULL,
+
+    CONSTRAINT "Professional_pkey" PRIMARY KEY ("id")
+);
