ea1fcfcecb14cb9d8598f1d1f45a51f7f08219f6
pedrohelias <pedrohelias95@hotmail.com>
2026-09-14 11:25:49 -0300

    login funcional!!

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,21 +1,59 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient()
 
-const register = async (req, res) => {
+// const register = async (req, res) => {
 
+//     try{
+//         const {name, email, cpf} = req.body
+//         const register = await prisma.patient.create({
+//             name, email, cpf
+//         })
+//     }catch(err){
+
+//     }
+// }
+
+
+const login = async (req, res) => {
     try{
-        const {name, email, cpf} = req.body
-        const register = await prisma.patient.create({
-            name, email, cpf
+        const {email, password} = req.body
+        const patient = await prisma.patient.findUnique({
+            where: {email: email}
         })
-    }catch(err){
 
+        if (!patient){
+            return res.status(401).json({
+                erro: "email ou senha inválidos!!"
+            })
+        }
+
+       /*  return res.json({
+            mensagem: "Usuario existe", patient
+        }) */
+
+        //verificação de senha
+
+        const isPasswordValid = await bcrypt.compare(password, patient.password)
+
+        if (!isPasswordValid){
+            return res.status(401).json({
+                error: "email ou senha inválidos!!"
+            })
+        }
+
+        res.json({status: "Success Login",
+            patient})
+
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({
+            error: "Erro ao logar"
+        })
     }
 }
 
 
-
-
-
-export {register}
+export {login}
